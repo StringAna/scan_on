d88988da81748e3aaabb82a093d3ec9be0160356
StringAna <tewaryantara21@gmail.com>
2020-02-10 11:10:57 +0530

    change1

---
+++ app/src/main/java/com/project/scan_on/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.project.scan_on;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        frameLayout=findViewById(R.id.register_framelayout);
+        setFragment(new SignInFragment());
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slideout_from_right);
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+}
