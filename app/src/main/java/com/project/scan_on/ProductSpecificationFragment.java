2c9fd202c8732038e3ab64f6d41f6381e0a74794
ankit <ankitkr8540@gmail.com>
2020-02-13 20:39:09 +0530

    ankit video 25

--- app/src/main/java/com/project/scan_on/ProductSpecificationFragment.java
+++ app/src/main/java/com/project/scan_on/ProductSpecificationFragment.java
@@ -4,10 +4,15 @@ package com.project.scan_on;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -20,12 +25,58 @@ public class ProductSpecificationFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private RecyclerView productSpecificationRecyclerview;
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_specification, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_specification, container, false);
+
+        productSpecificationRecyclerview = view.findViewById(R.id.product_specification_recyclerview);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+
+        productSpecificationRecyclerview.setLayoutManager(linearLayoutManager);
+
+
+        List<ProductSpecificationModel> productSpecificationModelList = new ArrayList<>();
+        productSpecificationModelList.add(new ProductSpecificationModel(0,"General"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(0,"Display"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(0,"General"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(0,"Display"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+        productSpecificationModelList.add(new ProductSpecificationModel(1,"RAM","4GB"));
+
+
+        ProductSpecificationAdapter productSpecificationAdapter = new ProductSpecificationAdapter(productSpecificationModelList);
+        productSpecificationRecyclerview.setAdapter(productSpecificationAdapter);
+        productSpecificationAdapter.notifyDataSetChanged();
+
+
+        return view;
     }
 
 }
