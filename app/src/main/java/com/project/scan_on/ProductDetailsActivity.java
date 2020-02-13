fdc7ba782ee348f6c4cf6e22a069ff47579cc869
ankit <ankitkr8540@gmail.com>
2020-02-13 13:17:52 +0530

    ankit video 23

--- app/src/main/java/com/project/scan_on/ProductDetailsActivity.java
+++ app/src/main/java/com/project/scan_on/ProductDetailsActivity.java
@@ -1,14 +1,95 @@
 package com.project.scan_on;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+
+    private ViewPager productImagesViewPager;
+    private TabLayout viewPagerIndicator;
+
+    private static boolean ALREADY_ADDED_TO_WISHLIST = false;
+    private FloatingActionButton addToWishlistBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        productImagesViewPager = findViewById(R.id.product_images_viewpager);
+        viewPagerIndicator = findViewById(R.id.view_pager_indicator);
+        addToWishlistBtn = findViewById(R.id.add_to_wishlist_btn);
+
+        List<Integer> productImages = new ArrayList<>();
+        productImages.add(R.mipmap.image2);
+        productImages.add(R.mipmap.stripadd);
+        productImages.add(R.mipmap.app_icon);
+        productImages.add(R.mipmap.banner);
+
+        ProductImagesAdapter productImagesAdapter = new ProductImagesAdapter(productImages);
+        productImagesViewPager.setAdapter(productImagesAdapter);
+
+
+        viewPagerIndicator.setupWithViewPager(productImagesViewPager,true);
+
+        addToWishlistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ALREADY_ADDED_TO_WISHLIST){
+                    ALREADY_ADDED_TO_WISHLIST =false;
+                    addToWishlistBtn.setSupportImageTintList(ColorStateList.valueOf(Color.parseColor("#9e9e9e")));
+
+                }else{
+                    ALREADY_ADDED_TO_WISHLIST =true;
+                    addToWishlistBtn.setSupportImageTintList(getResources().getColorStateList(R.color.colorPrimary));
+
+                }
+
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_and_cart_icon, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home ){
+            finish();
+            return true;
+        }else if (id == R.id.main_search_icon){
+            //todo:Notification
+            return true;
+        }else if (id == R.id.main_cart_icon){
+            //todo:cart icon
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
