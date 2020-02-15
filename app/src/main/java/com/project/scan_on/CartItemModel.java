24e4f081ae04d914e207ee95d7879c1142e9a436
ankit <ankitkr8540@gmail.com>
2020-02-15 09:07:11 +0530

    ankit video 29 13:09

---
+++ app/src/main/java/com/project/scan_on/CartItemModel.java
@@ -0,0 +1,142 @@
+package com.project.scan_on;
+
+public class CartItemModel {
+
+    public static   final int cCART_ITEM =0;
+    public static final int TOTAL_AMOUNT =1;
+
+    private int type;
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    //////////cart item
+    private int productImage;
+    private String productTitle;
+    private int freeCoupons;
+    private String cuttedPrice;
+    private String productPrice;
+    private int productQuantity;
+    private int offersApplied;
+    private int couponsApplied;
+
+
+
+    public CartItemModel(int type, int productImage, String productTitle, int freeCoupons, String cuttedPrice, String productPrice, int productQuantity, int offersApplied, int couponsApplied) {
+        this.type = type;
+
+
+       this.productImage = productImage;
+        this.productTitle = productTitle;
+        this.freeCoupons = freeCoupons;
+        this.cuttedPrice = cuttedPrice;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+        this.offersApplied = offersApplied;
+       this.couponsApplied = couponsApplied;
+    }
+
+    public int getProductImage() {
+        return productImage;
+    }
+    public void setProductImage(int productImage) {
+        this.productImage = productImage;
+    }
+    public String getProductTitle() {
+        return productTitle;
+    }
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+    public int getFreeCoupons() {
+        return freeCoupons;
+    }
+    public void setFreeCoupons(int freeCoupons) {
+        this.freeCoupons = freeCoupons;
+    }
+    public String getCuttedPrice() {
+        return cuttedPrice;
+    }
+    public void setCuttedPrice(String cuttedPrice) {
+        this.cuttedPrice = cuttedPrice;
+    }
+    public String getProductPrice() {
+        return productPrice;
+    }
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+    public int getOffersApplied() {
+        return offersApplied;
+    }
+    public void setOffersApplied(int offersApplied) {
+        this.offersApplied = offersApplied;
+    }
+    public int getCouponsApplied() {
+        return couponsApplied;
+    }
+    public void setCouponsApplied(int couponsApplied) {
+        this.couponsApplied = couponsApplied;
+    }
+
+    ///////// cart item
+
+    /////////cart total
+    private int totalItems;
+    private String totalAmount;
+    private String deliveryPrice;
+    private String savedAmount;
+
+    public CartItemModel(int type, int totalItems, String totalAmount, String deliveryPrice, String savedAmount) {
+        this.type = type;
+        this.totalItems = totalItems;
+        this.totalAmount = totalAmount;
+        this.deliveryPrice = deliveryPrice;
+        this.savedAmount = savedAmount;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(int totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getDeliveryPrice() {
+        return deliveryPrice;
+    }
+
+    public void setDeliveryPrice(String deliveryPrice) {
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    public String getSavedAmount() {
+        return savedAmount;
+    }
+
+    public void setSavedAmount(String savedAmount) {
+        this.savedAmount = savedAmount;
+    }
+    ////////cart total
+
+
+}
